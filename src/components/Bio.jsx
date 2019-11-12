c3d50d5b6e58a8398df15e1064190e805aa0d4ce
Hailey Gaylor and Jacqueline Remmel <jacqueline@>
2019-11-12 14:27:38 -0800

    Add image and update styling

--- src/components/Bio.jsx
+++ src/components/Bio.jsx
@@ -1,14 +1,28 @@
 import React from 'react';
+import duck from '../assets/images/duck.jpg';
 
 function Bio() {
   var bioStyle = {
     padding: '10',
-    borderColor: '#27253d'
+    borderColor: '#27253d',
+    
   };
+  var imgStyle = {
+    width: '100%',
+    height: 'auto',
+    display: 'block',
+    marginLeft: 'auto',
+    marginRight: 'auto',
+    borderRadius: '5px'
+  }
   return (
     <div
       style={bioStyle}
       className="card">
+      <img
+        style={imgStyle}
+        src={duck}/>
+      <br/>
       <p>This is my bio</p>
     </div>
   );
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,14 @@
 import React from 'react';
 
 function Header() {
+  var headerStyle = {
+    color: '#27253d',
+    textAlign: 'center',
+    marginTop: '20px',
+    marginBottom: '25px'
+  };
   return (
-    <h1>Quacker</h1>
+    <h1 style={headerStyle}>Quacker</h1>
   );
 }
 
